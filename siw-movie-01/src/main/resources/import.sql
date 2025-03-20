3dde13d2d2b6f9ef044ab3febceca5c409085075
compu <compu@DELL-5640>
2025-03-20 11:57:23 +0100

    Added a new Artist in the db

--- siw-movie-01/src/main/resources/import.sql
+++ siw-movie-01/src/main/resources/import.sql
@@ -1,5 +1,4 @@
 --Inserimento dei film in tabella tramite comandi SQL
-
 INSERT INTO movie (id, title, year, url_image) VALUES (nextval('movie_seq'), 'Inception', 2010, 'https://m.media-amazon.com/images/M/MV5BODI2M2FjNzItY2Q3Zi00NzdjLThmYTktY2Q2YzdiMGViMTc1XkEyXkFqcGc@._V1_.jpg');
 INSERT INTO movie (id, title, year, url_image) VALUES (nextval('movie_seq'), 'Interstellar',2014, 'https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg');
 INSERT INTO movie (id, title, year, url_image) VALUES (nextval('movie_seq'), 'The Dark Knight', 2008, 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg');
@@ -8,3 +7,4 @@ INSERT INTO movie (id, title, year, url_image) VALUES (nextval('movie_seq'), 'Ti
 
 --Inserimento di artisti in tabella tramite comandi SQL
 INSERT INTO artist (id, name, surname, birth) VALUES (nextval('artist_seq'), 'Christopher', 'Nolan', '1970-07-30');
+INSERT INTO artist (id, name, surname, birth) VALUES (nextval('artist_seq'), 'James', 'Cameron', '1954-08-16');
